c65f3b910bd371bb91eeedf7262eee29e2d54fc0
Eric Garcia <egarcia1868@gmail.com>
2020-12-16 13:49:58 -0600

    starting React Employee Directory over again from scratch

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,9 @@
-// import axios from "axios";
-// const BASEURL = "https://www.omdbapi.com/?t=";
-// const APIKEY = "&apikey=trilogy";
 import db from "../db/heroes.json"
 
-export default {
-  search: function(query) {
-    console.log(query)
-    return db.filter(hero => hero.name.toLowerCase().includes(query.toLowerCase()))
-    // return axios.get(BASEURL + query + APIKEY);
-  }
-};
+// export default {
+//   search: function(query) {
+//     console.log(query)
+//     return db.filter(hero => hero.name.toLowerCase().includes(query.toLowerCase()))
+//     // return axios.get(BASEURL + query + APIKEY);
+//   }
+// };
