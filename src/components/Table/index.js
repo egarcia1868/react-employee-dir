9894a235bd53788bbd058f44e2efaa790efc53b5
Eric Garcia <egarcia1868@gmail.com>
2020-12-16 17:53:00 -0600

    hero table generating correctly now.  Now to create search entry field.  Then on to creating a sorter

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -8,17 +8,93 @@ const Table = (props) => {
         <tr>
           <th>Photo</th>
           <th>Name</th>
-          {/*<th>Stats</th>        adding this one once I find out how to do the stat spacing*/}
+          <th>
+            Power Stats
+            <table style={{border: "none"}}>
+              <thead>
+                <tr>
+                  <th className="statsL">
+                    Int
+                  </th>
+                  <th className="statsL">
+                    Str
+                  </th>
+                  <th className="statsL">
+                    Spd
+                  </th>
+                  <th className="statsL">
+                    Dur
+                  </th>
+                  <th className="statsL">
+                    Pow
+                  </th>
+                  <th className="statsR">
+                    Combat
+                  </th>
+                </tr>
+              </thead>
+            </table>
+          </th>
           <th>Alignment</th>
         </tr>
       </thead>
       <tbody>
         {props.fullList.map(hero => 
-          <tr>
-          <td><img style={{width: "75px"}} src={hero.image.url} alt=""/></td>
-          <td>{hero.name}</td>
-          <td>{hero.biography.alignment}</td>
-        </tr>
+          <tr key={hero.id}>
+            <td><img src={hero.image.url} alt=""/></td>
+            <td>{hero.name}</td>
+            <td>
+              <table style={{border: "none"}}>
+                <thead>
+                  <tr>
+                    <th className="statsL">
+                      Int
+                    </th>
+                    <th className="statsL">
+                      Str
+                    </th>
+                    <th className="statsL">
+                      Spd
+                    </th>
+                    <th className="statsL">
+                      Dur
+                    </th>
+                    <th className="statsL">
+                      Pow
+                    </th>
+                    <th className="statsR">
+                      Combat
+                    </th>
+                  </tr>
+                </thead>
+              </table>
+              <table style={{border: "none"}}>
+                <thead>
+                  <tr>
+                    <th className="statsL">
+                      {props.handleNull(hero.powerstats.intelligence)}
+                    </th>
+                    <th className="statsL">
+                      {props.handleNull(hero.powerstats.strength)}
+                    </th>
+                    <th className="statsL">
+                      {props.handleNull(hero.powerstats.speed)}
+                    </th>
+                    <th className="statsL">
+                      {props.handleNull(hero.powerstats.durability)}
+                    </th>
+                    <th className="statsL">
+                      {props.handleNull(hero.powerstats.power)}
+                    </th>
+                    <th className="statsR">
+                      {props.handleNull(hero.powerstats.combat)}
+                    </th>
+                  </tr>
+                </thead>
+              </table>
+            </td>
+            <td>{hero.biography.alignment}</td>
+          </tr>
         )}
       </tbody>
     </table>
