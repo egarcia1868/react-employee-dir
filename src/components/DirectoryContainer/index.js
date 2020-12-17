a62b20ab3fab8e9265673c13e7113c4a8b41153b
Eric Garcia <egarcia1868@gmail.com>
2020-12-17 12:36:42 -0600

    directory is now sorting by name and alignment, now to start sorting by stats

--- src/components/DirectoryContainer/index.js
+++ src/components/DirectoryContainer/index.js
@@ -3,13 +3,13 @@ import Table from "../Table";
 import SearchForm from "../SearchForm";
 import DB from "../../db/heroes.json";
 
-
 class Directory extends Component {
   state ={
     //This will save all the super heroes that match whats entered into the search field.
     filteredList: [],
     search: "",
-    filtered: false
+    sorter: ["alignment","asc"],
+    // alignment: [[],[],[]]
   }
 
   // This will change what is shown in the search field as you type
@@ -22,6 +22,64 @@ class Directory extends Component {
     event.preventDefault();
     //this will create a new array of supes that include the searched entry
     this.setState({filteredList: DB.filter(supe => supe.name.includes(this.state.search))})
+  }
+
+  //  NEED TO EXPAND UPON THIS FOR POWER STATS.  WILL ONLY WORK WITH NAME AND AFFINITY RIGHT NOW
+  handleSorter = event => {
+    const sortBy = event.target.dataset.sortBy;
+
+    // This function is to exercise DIY since this function framework will be used a lot in the resorting
+
+    //THIS IS NOT WORKING.  MAY COME BACK!!!!!!! 
+
+    // const orderFlipper = (num1, num2, category, order) => {
+    //   this.setState({filteredList: this.state.filteredList.sort((a, b) => (a.category > b.category) ? num1 : num2), sorter: [category, order]})
+    // }
+
+    // if (sortBy === this.state.sorter[0]) {
+
+
+      // Since some of the things we'll be sorting by are more nested in the JSON than others, we need to do different things to sort depending on which is clicked.  This is where the switch statement comes in.
+      switch(sortBy) {
+        case "name":
+          // This will determine which way the chart is currently being sorted and reverse it
+          if (this.state.sorter[1] === "asc") {
+            // orderFlipper(-1, 1, "name", "des");
+            
+            // Below is essentially what the above code equates to
+            this.setState({filteredList: this.state.filteredList.sort((a, b) => (a.name > b.name) ? -1 : 1), sorter: ["name", "des"]})
+          } else {
+            // orderFlipper(1, -1, "name", "asc");
+            this.setState({filteredList: this.state.filteredList.sort((a, b) => (a.name > b.name) ? 1 : -1), sorter: ["name", "asc"]})
+          }
+          break;
+        case "alignment":
+          const first = [];
+          const second = [];
+          const third = [];
+          const filteredList = this.state.filteredList
+          // this will seperate the supes into groups based on their alignment
+          for(let i = 1; i < filteredList.length; i++) {
+            if (filteredList[i].biography.alignment === "bad") {
+              first.push(filteredList[i])
+            } else if (filteredList[i].biography.alignment === "neutral") {
+              second.push(filteredList[i])
+            } else {
+              third.push(filteredList[i])
+            }
+          }
+          if (this.state.sorter[1] === "asc") {
+            this.setState({filteredList: first.concat(second, third), sorter: ["alignment", "des"]})
+          } else {
+            this.setState({filteredList: third.concat(second, first), sorter: ["alignment", "asc"]})
+          }
+          break;
+        default:
+          break;
+      // }
+
+      
+    }
   }
   
 // If a power stat is listed as "null", this will change it to unk before adding it the directory
@@ -39,7 +97,7 @@ class Directory extends Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>
           Welcomd to Seek-a-Supe!
         </h1>
@@ -54,8 +112,10 @@ class Directory extends Component {
           handleFormSubmit={this.handleFormSubmit}
         />
         <Table 
+          handleSorter={this.handleSorter}
           handleNull={this.handleNull}
           filteredList={this.state.filteredList}
+          // checkForOrder={this.checkForOrder}
         />
       </div>
     )
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,101 +3,104 @@ import "./style.css";
 
 const Table = (props) => {
   return (
-    <table id="supeTable">
-      <thead>
-        <tr>
-          <th>Photo</th>
-          <th>Name</th>
-          <th>
-            Power Stats
-            <table style={{border: "none"}}>
-              <thead>
-                <tr>
-                  <th className="statsL">
-                    Int
-                  </th>
-                  <th className="statsL">
-                    Str
-                  </th>
-                  <th className="statsL">
-                    Spd
-                  </th>
-                  <th className="statsL">
-                    Dur
-                  </th>
-                  <th className="statsL">
-                    Pow
-                  </th>
-                  <th className="statsR">
-                    Combat
-                  </th>
-                </tr>
-              </thead>
-            </table>
-          </th>
-          <th>Alignment</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.filteredList.map(hero => 
-          <tr key={hero.id}>
-            <td><img src={hero.image.url} alt=""/></td>
-            <td>{hero.name}</td>
-            <td>
+    <div>
+      <p>Click on any of the categories (except "Photo") to reorganize the current list.  Click on each individual Power Stat to reorganize</p>
+      <table id="supeTable">
+        <thead>
+          <tr>
+            <th>Photo</th>
+            <th data-sort-by="name" onClick={props.handleSorter}>Name</th>
+            <th>
+              Power Stats
               <table style={{border: "none"}}>
                 <thead>
                   <tr>
-                    <th className="statsL">
+                    <th data-sort-by="intelligence" onClick={props.handleSorter} className="statsL">
                       Int
                     </th>
-                    <th className="statsL">
+                    <th data-sort-by="strength" onClick={props.handleSorter} className="statsL">
                       Str
                     </th>
-                    <th className="statsL">
+                    <th data-sort-by="speed" onClick={props.handleSorter} className="statsL">
                       Spd
                     </th>
-                    <th className="statsL">
+                    <th data-sort-by="durability" onClick={props.handleSorter} className="statsL">
                       Dur
                     </th>
-                    <th className="statsL">
+                    <th data-sort-by="power" onClick={props.handleSorter} className="statsL">
                       Pow
                     </th>
-                    <th className="statsR">
+                    <th data-sort-by="combat" onClick={props.handleSorter} className="statsR">
                       Combat
                     </th>
                   </tr>
                 </thead>
               </table>
-              <table style={{border: "none"}}>
-                <thead>
-                  <tr>
-                    <th className="statsL">
-                      {props.handleNull(hero.powerstats.intelligence)}
-                    </th>
-                    <th className="statsL">
-                      {props.handleNull(hero.powerstats.strength)}
-                    </th>
-                    <th className="statsL">
-                      {props.handleNull(hero.powerstats.speed)}
-                    </th>
-                    <th className="statsL">
-                      {props.handleNull(hero.powerstats.durability)}
-                    </th>
-                    <th className="statsL">
-                      {props.handleNull(hero.powerstats.power)}
-                    </th>
-                    <th className="statsR">
-                      {props.handleNull(hero.powerstats.combat)}
-                    </th>
-                  </tr>
-                </thead>
-              </table>
-            </td>
-            <td>{hero.biography.alignment}</td>
+            </th>
+            <th data-sort-by="alignment" onClick={props.handleSorter}>Alignment</th>
           </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {props.filteredList.map(hero => 
+            <tr key={hero.id}>
+              <td><img src={hero.image.url} alt=""/></td>
+              <td>{hero.name}</td>
+              <td>
+                <table style={{border: "none"}}>
+                  <thead>
+                    <tr>
+                      <th className="statsL">
+                        Int
+                      </th>
+                      <th className="statsL">
+                        Str
+                      </th>
+                      <th className="statsL">
+                        Spd
+                      </th>
+                      <th className="statsL">
+                        Dur
+                      </th>
+                      <th className="statsL">
+                        Pow
+                      </th>
+                      <th className="statsR">
+                        Combat
+                      </th>
+                    </tr>
+                  </thead>
+                </table>
+                <table style={{border: "none"}}>
+                  <thead>
+                    <tr>
+                      <th className="statsL">
+                        {props.handleNull(hero.powerstats.intelligence)}
+                      </th>
+                      <th className="statsL">
+                        {props.handleNull(hero.powerstats.strength)}
+                      </th>
+                      <th className="statsL">
+                        {props.handleNull(hero.powerstats.speed)}
+                      </th>
+                      <th className="statsL">
+                        {props.handleNull(hero.powerstats.durability)}
+                      </th>
+                      <th className="statsL">
+                        {props.handleNull(hero.powerstats.power)}
+                      </th>
+                      <th className="statsR">
+                        {props.handleNull(hero.powerstats.combat)}
+                      </th>
+                    </tr>
+                  </thead>
+                </table>
+              </td>
+              <td>{hero.biography.alignment}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
