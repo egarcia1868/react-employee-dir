ff73067f18b293eb9bcd35d2fd204a345446a031
Eric Garcia <egarcia1868@gmail.com>
2020-12-31 14:58:29 -0600

    the sorter will now always go in descending order first unless the same category is clicked twice

--- src/components/DirectoryContainer/index.js
+++ src/components/DirectoryContainer/index.js
@@ -1,29 +1,28 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import Table from "../Table";
 import SearchForm from "../SearchForm";
 import DB from "../../db/heroes.json";
 
-class Directory extends Component {
-  state ={
+const Directory = () => {
     //This will save all the super heroes that match whats entered into the search field.
-    filteredList: [],
-    search: "",
-    sorter: "asc",
-  }
+  const [filteredList, setFilteredList] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sorter, setSorter] = useState("asc");
+  const [category, setCategory] = useState("name");
 
   // This will change what is shown in the search field as you type
-  handleInputChange = event => {
+  const handleInputChange = event => {
     const value = event.target.value;
-    this.setState({ search: value })
+    setSearch( value )
   }
 
-  handleFormSubmit = event => {
+  const handleFormSubmit = event => {
     event.preventDefault();
     //this will create a new array of supes that include the searched entry
-    this.setState({filteredList: DB.filter(supe => supe.name.toLowerCase().includes(this.state.search.toLowerCase()))})
+    setFilteredList( DB.filter(supe => supe.name.toLowerCase().includes(search.toLowerCase())))
   }
 
-  statComparer = (list, statBase, stat, num1, num2) => {
+  const statComparer = (list, statBase, stat, num1, num2) => {
 
     return list.sort((a, b) => {
       // This will check to see if the stat is a number or "unk".  If "unk" it will send that supe to the bottom
@@ -36,9 +35,8 @@ class Directory extends Component {
     })
   }
 
-  handleSorter = event => {
+  const handleSorter = event => {
     const sortBy = event.target.dataset.sortBy;
-    let filteredList = this.state.filteredList;
 
     // This function is to exercise DIY since this function framework will be used a lot in the resorting
 
@@ -49,19 +47,16 @@ class Directory extends Component {
     // }
 
 
+    // This will determine which way the chart is currently being sorted and reverse it
+    if (sorter === "asc" || sortBy !== category) {
+      setSorter( "des" );
       // Since some of the things we'll be sorting by are more nested in the JSON than others, we need to do different things to sort depending on which is clicked.  This is where the switch statement comes in.
       switch(sortBy) {
         case "name":
-          // This will determine which way the chart is currently being sorted and reverse it
-          if (this.state.sorter === "asc") {
-            // orderFlipper(-1, 1, "name", "des");
-            
-            this.setState({filteredList: this.state.filteredList.sort((a, b) => (a.name > b.name) ? -1 : 1), sorter: "des"})
-          } else {
-            // orderFlipper(1, -1, "name", "asc");
-            this.setState({filteredList: this.state.filteredList.sort((a, b) => (a.name > b.name) ? 1 : -1), sorter: "asc"})
-          }
+          setFilteredList( filteredList.sort((a, b) => (a.name > b.name) ? -1 : 1));
+          setCategory("name");
           break;
+
         case "alignment":
           const first = [];
           const second = [];
@@ -75,64 +70,195 @@ class Directory extends Component {
             } else {
               third.push(filteredList[i])
             }
-          }
-          if (this.state.sorter === "asc") {
-            this.setState({filteredList: first.concat(second, third), sorter: "des"})
-          } else {
-            this.setState({filteredList: third.concat(second, first), sorter: "asc"})
-          }
-          break;
-        case "intelligence":
-          if (this.state.sorter === "asc") {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "intelligence", -1, 1), sorter: "des"})
-          } else {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "intelligence", 1, -1), sorter: "asc"})
-          }
-          break;
-        case "strength":
-          if (this.state.sorter === "asc") {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "strength", -1, 1), sorter: "des"})
-          } else {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "strength", 1, -1), sorter: "asc"})
-          }
-          break;
-        case "speed":
-          if (this.state.sorter === "asc") {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "speed", -1, 1), sorter: "des"})
-          } else {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "speed", 1, -1), sorter: "asc"})
-          }
-          break;
-        case "durability":
-          if (this.state.sorter === "asc") {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "durability", -1, 1), sorter: "des"})
-          } else {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "durability", 1, -1), sorter: "asc"})
-          }
-          break;
-        case "power":
-          if (this.state.sorter === "asc") {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "power", -1, 1), sorter: "des"})
-          } else {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "power", 1, -1), sorter: "asc"})
-          }
-          break;
-        case "combat":
-          if (this.state.sorter === "asc") {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "combat", -1, 1), sorter: "des"})
-          } else {
-            this.setState({filteredList: this.statComparer(filteredList, "powerstats", "combat", 1, -1), sorter: "asc"})
-          }
-          break;
-        default:
+          };
+          setFilteredList( third.concat(second, first));
+          setCategory("alignment");
           break;
 
-      
+        case "intelligence":
+          setFilteredList( statComparer(filteredList, "powerstats", "intelligence", -1, 1));
+          setCategory("intelligence");
+          break;
+
+        case "strength":
+          setFilteredList( statComparer(filteredList, "powerstats", "strength", -1, 1));
+          setCategory("strength");
+          break;
+          
+        case "speed":
+          setFilteredList( statComparer(filteredList, "powerstats", "speed", -1, 1));
+          setCategory("speed");
+          break;
+
+        case "durability":
+          setFilteredList( statComparer(filteredList, "powerstats", "durability", -1, 1));
+          setCategory("durability");
+          break;
+
+        case "power":
+          setFilteredList( statComparer(filteredList, "powerstats", "power", -1, 1));
+          setCategory("power");
+          break;
+
+        case "combat":
+          setFilteredList( statComparer(filteredList, "powerstats", "combat", -1, 1));
+          setCategory("combat");
+          break;
+
+        default:
+          break;
+      }
+    } else {
+      setSorter( "asc" );
+      switch(sortBy) {
+        case "name":
+          setFilteredList( filteredList.sort((a, b) => (a.name > b.name) ? 1 : -1));
+          // setCategory("name");
+          break;
+
+        case "alignment":
+          const first = [];
+          const second = [];
+          const third = [];
+          // this will seperate the supes into groups based on their alignment
+          for(let i = 1; i < filteredList.length; i++) {
+            if (filteredList[i].biography.alignment === "bad") {
+              first.push(filteredList[i])
+            } else if (filteredList[i].biography.alignment === "neutral") {
+              second.push(filteredList[i])
+            } else {
+              third.push(filteredList[i])
+            }
+          };
+          setFilteredList( first.concat(second, third));
+          // setCategory("alignment");
+          break;
+
+        case "intelligence":
+          setFilteredList( statComparer(filteredList, "powerstats", "intelligence", 1, -1));
+          // setCategory("intelligence");
+          break;
+
+        case "strength":
+          setFilteredList( statComparer(filteredList, "powerstats", "strength", 1, -1));
+          // setCategory("strength");
+          break;
+          
+        case "speed":
+          setFilteredList( statComparer(filteredList, "powerstats", "speed", 1, -1));
+          // setCategory("speed");
+          break;
+
+        case "durability":
+          setFilteredList( statComparer(filteredList, "powerstats", "durability", 1, -1));
+          // setCategory("durability");
+          break;
+
+        case "power":
+          setFilteredList( statComparer(filteredList, "powerstats", "power", 1, -1));
+          // setCategory("power");
+          break;
+
+        case "combat":
+          setFilteredList( statComparer(filteredList, "powerstats", "combat", 1, -1));
+          // setCategory("combat");
+          break;
+
+        default:
+          break;
+      }
     }
   }
+
+
+
+//       switch(sortBy) {
+//         case "name":
+//           // This will determine which way the chart is currently being sorted and reverse it
+//           if (sorter === "asc") {
+//             // orderFlipper(-1, 1, "name", "des");
+            
+//             setFilteredList( filteredList.sort((a, b) => (a.name > b.name) ? -1 : 1))
+//             setSorter( "des" )
+//           } else {
+//             // orderFlipper(1, -1, "name", "asc");
+//             setFilteredList( filteredList.sort((a, b) => (a.name > b.name) ? 1 : -1))
+//             setSorter( "asc")
+//           }
+//           break;
+//         case "alignment":
+//           const first = [];
+//           const second = [];
+//           const third = [];
+//           // this will seperate the supes into groups based on their alignment
+//           for(let i = 1; i < filteredList.length; i++) {
+//             if (filteredList[i].biography.alignment === "bad") {
+//               first.push(filteredList[i])
+//             } else if (filteredList[i].biography.alignment === "neutral") {
+//               second.push(filteredList[i])
+//             } else {
+//               third.push(filteredList[i])
+//             }
+//           }
+//           if (sorter === "asc") {
+//             setFilteredList( first.concat(second, third) )
+//             setSorter( "des" )
+//           } else {
+//             setFilteredList( third.concat(second, first))
+//             setSorter( "asc" )
+//           }
+//           break;
+//         case "intelligence":
+//           if (sorter === "asc") {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "intelligence", -1, 1), sorter: "des"})
+//           } else {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "intelligence", 1, -1), sorter: "asc"})
+//           }
+//           break;
+//         case "strength":
+//           if (this.state.sorter === "asc") {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "strength", -1, 1), sorter: "des"})
+//           } else {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "strength", 1, -1), sorter: "asc"})
+//           }
+//           break;
+//         case "speed":
+//           if (this.state.sorter === "asc") {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "speed", -1, 1), sorter: "des"})
+//           } else {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "speed", 1, -1), sorter: "asc"})
+//           }
+//           break;
+//         case "durability":
+//           if (this.state.sorter === "asc") {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "durability", -1, 1), sorter: "des"})
+//           } else {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "durability", 1, -1), sorter: "asc"})
+//           }
+//           break;
+//         case "power":
+//           if (this.state.sorter === "asc") {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "power", -1, 1), sorter: "des"})
+//           } else {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "power", 1, -1), sorter: "asc"})
+//           }
+//           break;
+//         case "combat":
+//           if (this.state.sorter === "asc") {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "combat", -1, 1), sorter: "des"})
+//           } else {
+//             this.setState({filteredList: this.statComparer(filteredList, "powerstats", "combat", 1, -1), sorter: "asc"})
+//           }
+//           break;
+//         default:
+//           break;
+
+      
+//     }
+//   }
   
 // If a power stat is listed as "null", this will change it to unk before adding it the directory
-  handleNull = (heroStat) => {
+  const handleNull = (heroStat) => {
     if (heroStat === "null") {
       return "unk"
     } else {
@@ -140,35 +266,34 @@ class Directory extends Component {
     }
   }
 
-  componentDidMount() {
-    this.setState({filteredList: DB})
-  }
+  useEffect(() => {
+    setFilteredList( DB )
+  }, [])
 
-  render() {
-    return (
-      <div className="container">
-        <h1>
-          Welcomd to Seek-a-Supe!
-        </h1>
-        <h2>
-          Got a super problem that needs to be dealt with?  Search our database of super heroes (and villians if the task is a little on the shady side) and find someone better suited to handle it:
-        </h2>
-        <SearchForm 
-          handleInputChange={this.handleInputChange}
-          value={this.state.search}
-          filteredList={this.state.filteredList}
-          fullList={DB}
-          handleFormSubmit={this.handleFormSubmit}
-        />
-        <Table 
-          handleSorter={this.handleSorter}
-          handleNull={this.handleNull}
-          filteredList={this.state.filteredList}
-          // checkForOrder={this.checkForOrder}
-        />
-      </div>
-    )
-  }
+
+  return (
+    <div className="container">
+      <h1>
+        Welcomd to Seek-a-Supe!
+      </h1>
+      <h2>
+        Got a super problem that needs to be dealt with?  Search our database of super heroes (and villians if the task is a little on the shady side) and find someone better suited to handle it:
+      </h2>
+      <SearchForm 
+        handleInputChange={handleInputChange}
+        value={search}
+        filteredList={filteredList}
+        fullList={DB}
+        handleFormSubmit={handleFormSubmit}
+      />
+      <Table 
+        handleSorter={handleSorter}
+        handleNull={handleNull}
+        filteredList={filteredList}
+        // checkForOrder={this.checkForOrder}
+      />
+    </div>
+  )
 
 }
 
